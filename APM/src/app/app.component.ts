4a73ed6b9a619bbd81cca540bd454968e89b316a
oburisin <oburisin@hotmail.com>
2017-09-08 23:31:26 +0200

    Comment on back ticks comment

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root', // convention to prefix by part of app (pm - product management)
-  template: `<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>`
+  template: `<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>` // back ticks allow multi line strings, '' or "" are for inline
 })
 export class AppComponent {
   pageTitle: string =  'Acme Product Management'; 
