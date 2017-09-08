c3376abb52b4e2dfb950d7959bc580734ea9560f
oburisin <oburisin@hotmail.com>
2017-09-08 23:10:22 +0200

    Add the component to the module + use it in another component as a directive

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root', // convention to prefix by part of app (pm - product management)
-  template: `<div><h1>{{pageTitle}}</h1><div>My first component</div></div>`
+  template: `<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>`
 })
 export class AppComponent {
   pageTitle: string =  'Acme Product Management'; 
--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 // decorator <=> attribute in C#
 @NgModule({
   declarations: [
-    AppComponent  // array of what belongs to the module
+    AppComponent,  // array of what belongs to the module
+    ProductListComponent
   ],
   imports: [ // external modules be available to all components in this Angular module
     BrowserModule // important for all browser apps, contains error handling 
